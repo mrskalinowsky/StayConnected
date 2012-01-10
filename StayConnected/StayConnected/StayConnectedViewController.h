729fd9952c70b2dc4a23c17928b65066c45f25d1
Christian Smith <smith.cba@gmail.com>
2012-01-10 19:57:43 +0000

    Initial commit of xcode project

---
+++ StayConnected/StayConnected/StayConnectedViewController.h
@@ -0,0 +1,15 @@
+//
+//  StayConnectedViewController.h
+//  StayConnected
+//
+//  Created by Christian Smith on 10/01/2012.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StayConnectedViewController : UIViewController {
+    
+}
+
+@end
